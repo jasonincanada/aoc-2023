1ec50a48a2de872b15f10efa5ce5321a1bf7d36c
jasonincanada <nirgle@gmail.com>
2023-12-05 19:41:17 -0500

    update template.rs

--- template.rs
+++ template.rs
@@ -1,11 +1,12 @@
 // https://adventofcode.com/2023/day/1
 
+use std::str::FromStr;
+
 fn main() -> Result<(), String> {
 
-    let sample1 = input_from("sample-1.txt")?;
-    let sample2 = input_from("sample-2.txt")?;
-    println!("Sample part 1: {}", part1(&sample1)); // 142
-    println!("Sample part 2: {}", part2(&sample2)); // 281
+    let sample = input_from("sample.txt")?;
+    println!("Sample part 1: {}", part1(&sample));
+    println!("Sample part 2: {}", part2(&sample));
 
     let input = input_from("input.txt")?;
     println!("Part 1: {}", part1(&input));
@@ -26,6 +27,9 @@ fn part2(input: &Input) -> u32 {
     0
 }
 
+
+/* Parsing */
+
 // Read input from a file
 fn input_from(file: &str) -> Result<Input, String> {
 
@@ -45,14 +49,27 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_split() {
-        let line = "467..114..";
-        let splits: Vec<&str> = line.split('.').collect();
+    fn test_sample_part1() {
+        let input = input_from("sample.txt").unwrap();
+        assert_eq!(0, part1(&input))
+    }
 
-        assert_eq!(5, splits.len());
-        assert_eq!("467", splits[0]);
-        assert_eq!(""   , splits[1]);
-        assert_eq!("114", splits[2]);
+    #[test]
+    fn test_sample_part2() {
+        let input = input_from("sample.txt").unwrap();
+        assert_eq!(0, part2(&input))
+    }
+
+    #[test]
+    fn test_part1() {
+        let input = input_from("input.txt").unwrap();
+        assert_eq!(0, part1(&input))
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = input_from("input.txt").unwrap();
+        assert_eq!(0, part2(&input))
     }
 }
 
