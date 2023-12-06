da6b0cab0a242555480ed4c5b6281b199bd19c2f
jasonincanada <nirgle@gmail.com>
2023-12-05 19:47:45 -0500

    add perl script to sum ranges in input

---
+++ days/day05/sum_ranges.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+#
+#  Sum the ranges on the first line of the input file. ChatGPT 4.0 wrote this with:
+#
+#  prompt: "Write a perl script to read the first line from a file, split on whitespace,
+#           discard the first token, then sum every 2nd token that is left"
+#  then  : "Can it read from stdin"
+#
+#   $ perl sum_ranges.pl < sample.txt
+#   Sum: 134
+#
+#   $ perl sum_ranges.pl < input.txt
+#   Sum: 17395126216
+#
+
+use strict;
+use warnings;
+
+# Read the first line
+my $line = <STDIN>;
+
+# Split the line on whitespace and remove the first token
+my @tokens = split(/\s+/, $line);
+shift @tokens;
+
+# Initialize sum
+my $sum = 0;
+
+# Iterate and sum every second token
+for (my $i = 0; $i < @tokens; $i += 2) {
+    $sum += $tokens[$i] if $tokens[$i] =~ /^\d+$/;
+}
+
+print "Sum: $sum\n";
